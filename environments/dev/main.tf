7c1eec9ec258d989df385dd6750bde3ae45a0899
Valdir Bussola Junior <valdirbussola@gmail.com>
2026-02-27 11:02:02 -0300

    feat: setup aws pipeline with tflint and checkov - tentativa 3

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,6 +6,19 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.32.1"
     }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
 
   backend "s3" {
